981e401be2344259edf468355a2980d59f415c5f
kawtarabd <biouikawtar@gmail.com>
2023-12-18 12:30:28 +0100

    update

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * insertion_sort_list - sorts a DLL of integers in
+ * ascending order using the insertion sort
+ * algorithm
+ *
+ * @list: doubly linked list
+ * Return: no return
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (!list)
+		return;
+
+	current = *list;
+
+	while (current)
+	{
+		while (current->next && (current->n > current->next->n))
+		{
+			temp = current->next;
+			current->next = temp->next;
+			temp->prev = current->prev;
+
+			if (current->prev)
+				current->prev->next = temp;
+
+			if (temp->next)
+				temp->next->prev = current;
+
+			current->prev = temp;
+			temp->next = current;
+
+			if (temp->prev)
+				current = temp->prev;
+			else
+				*list = temp;
+
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
